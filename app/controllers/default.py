76a2fb92511bf21a0e999eb37930fc7441234eea
Allef Fressatto <frrssatto@gmail.com>
2022-07-07 13:19:07 -0300

    Menu finalizado e inicio da pagina de account

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -4,3 +4,8 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/account/<user>")
+@app.route("/account/", defaults={'user':None})
+def acoount(user):
+    return render_template('account.html', user=user.title())
