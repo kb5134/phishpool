92861e0081143786d87fa45338067dc2eaac3dfc
kb5134 <frrssatto@gmail.com>
2022-07-29 00:37:20 -0300

    inicio do desenvolvimento do formulario de login

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,11 +1,17 @@
 from flask import render_template
 from app import app
+from app.models.formularios import loginForm
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    form = loginForm
+    return render_template("login.html", form=form)
 
 @app.route("/account/<user>")
 @app.route("/account/", defaults={'user':'user'})
 def acoount(user):
     return render_template('account.html', user=user)
+
+@app.route('/envio')
+def envio():
+    return render_template('enviar.html')
