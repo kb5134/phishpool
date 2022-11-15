05e19437e71344fc48cba9a2b488627d60934d01
kb5134 <frrssatto@gmail.com>
2022-11-15 20:17:38 -0300

    Inicio do desenvolvimento do scrapping, falta tratar o retorno dos preços, pois o método utilizado para retornar, é o mesmo que retorna as avaliações.

--- app/models/Scrappy.py
+++ app/models/Scrappy.py
@@ -3,14 +3,29 @@ from requests import request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 produto = input('insira o produto a ser buscado: ')
+
 
 navegador = webdriver.Firefox()
 
 navegador.get('https://shopping.google.com.br/')
 
 elemento = navegador.find_element(By.ID, 'REsRA')
+elemento.send_keys(produto)
+elemento.submit()
 
-elemento.send_keys(produto + Keys.ENTER)
+sleep(2)
+
+conteudo = navegador.page_source
+site = BeautifulSoup(conteudo, 'html.parser')
+
+sleep(2)
+produtos = site.findAll('div', attrs={'class':'sh-pr__product-results-grid sh-pr__product-results'})
+for produto in produtos:
+    for i in range(3):
+        preco_produto = produto.findAll('span', attrs={'aria-hidden':'true'})[i]
+        if 'R$' in preco_produto:
+            print(preco_produto.text)
 
